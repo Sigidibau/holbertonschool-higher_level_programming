a8ff2683048668750a769e26d11ee89f953ab044
sigidibau <9571@holbertonstudents.com>
2024-09-09 13:24:03 -0400

    copy list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
+def new_in_list(my_list, idx, new_element):
     if idx < 0 or idx > (len(my_list) - 1):
         return my_lsit
     else:
         copy_list = my_list.copy()
-        copy_list[idx] = element
+        copy_list[idx] = new_element
         return copy_list
