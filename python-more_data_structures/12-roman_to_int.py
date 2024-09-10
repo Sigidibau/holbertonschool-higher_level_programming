7330f146b12fbc9850b85b37bd362c814abd3fd4
sigidibau <9571@holbertonstudents.com>
2024-09-10 18:09:07 -0400

    roman numbers

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
-     roman_map = {
+    roman_map = {
         'I': 1,
         'V': 5,
         'X': 10,
@@ -10,9 +10,8 @@ def roman_to_int(roman_string):
         'C': 100,
         'D': 500,
         'M': 1000
-    }
+        }
     total, prev_value = 0, 0
-
     for char in reversed(roman_string):
         if char not in roman_map:
             return 0
