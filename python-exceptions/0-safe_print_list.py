e09ee52a25e8857fab0d80f82e359207092e5b17
sigidibau <9571@holbertonstudents.com>
2024-09-11 10:50:51 -0400

    safe print

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print('{:d}'.format(my_list[i]), end="")
             y += 1
-        except ValueError:
-            pass
+        except IndexError:
+            break
     print()
     return y
