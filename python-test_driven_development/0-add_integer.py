b19f70a6cfba77a725f8d40a4f23a5c0f7c5ea3d
sigidibau <9571@holbertonstudents.com>
2024-09-12 12:27:07 -0400

    remove a come in line 6 after b

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,5 +3,5 @@ def add_integer(a, b=98):
     if not isinstance(a, (int, float)):
         raise TypeError('a mustbe an integer')
     if not isinstance(b, (int, float)):
-        raise TypeError('b, must be an integer')
+        raise TypeError('b must be an integer')
     return int(a) + int(b)
