79d25dade0fe9b50a1bb1bbbde832bd17af5ede9
sigidibau <9571@holbertonstudents.com>
2024-09-16 12:42:39 -0400

    fix doc

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 class Square:
-    pass
-    """Class: is a Square
-    Args: none
+  """
+    Represents a square.
+
+    This class allows you to create a square with a specific size and
+    provides methods to calculate its area and obtain a string representation.
+
+    Attributes:
+        size (int): The size of the square's side.
+
+    Methods:
+        __init__(size):
+            Initializes a new Square instance with the specified size.
+        area():
+            Returns the area of the square.
+        __str__():
+            Returns a string representation of the square.
     """
+    pass
