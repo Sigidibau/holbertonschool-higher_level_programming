fcf8ada228d1c7be7e7b02e8821253cb34c10359
sigidibau <9571@holbertonstudents.com>
2024-09-16 23:50:20 -0400

    define a calss

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""cearting a class name Square"""
+
+
+class Square:
+    """class represent a square
+    """
+    
+    def __init__(self, size):
+        """Initialize a square.
+
+        Args:
+            size (int): private instance attribute.
+        """
+        self.__size = size
