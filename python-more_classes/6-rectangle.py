c93b6f44b25de042a876b2138ef51ab023c8d746
sigidibau <9571@holbertonstudents.com>
2024-09-19 12:34:51 -0400

    number of instantces

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -4,7 +4,7 @@
 
 class Rectangle:
     """Represent a rectangle"""
-    
+
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
@@ -17,7 +17,7 @@ class Rectangle:
          """
         self.width = width
         self.height = height
-        
+
         Rectangle.number_of_instances += 1
 
     @property
