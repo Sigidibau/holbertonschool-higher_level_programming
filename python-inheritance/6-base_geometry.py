9a4e09d7b8cabc955ba0c2ea1a31e108ebae0220
sigidibau <9571@holbertonstudents.com>
2024-09-23 14:07:09 -0400

    raising an exception in my class

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-"""BaseGeometry class that defines an interface for geometric shapes."""
+"""BaseGeometry class that defines an interface for geometric shapes.
+
+The BaseGeometry class is an abstract class that provides a blueprint
+for geometric shapes. It includes an abstract method `area`, which must
+be implemented by any subclass. This class is intended to be
+inherited by other classes that represent specific geometric figures.
+"""
 
 
 class BaseGeometry:
