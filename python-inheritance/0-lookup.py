3ac6bd5878cec63748ad88b65a3922699f6c179d
sigidibau <9571@holbertonstudents.com>
2024-09-23 12:34:45 -0400

    list of dir

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Return the list of available attributes and methods of an object."""
 
-    
+
 def lookup(obj):
-     """Return a list of available attributes and methods of an object."""
-     return dir(obj)
+    """Return a list of available attributes and methods of an object."""
+    return dir(obj)
