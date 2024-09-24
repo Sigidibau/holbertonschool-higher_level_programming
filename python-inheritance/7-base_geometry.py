7482706b873d479b2dfac3cae9f91004931a1555
sigidibau <9571@holbertonstudents.com>
2024-09-24 12:14:23 -0400

    fixes

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,52 +1,31 @@
-#!/usr/bin/python3
-"""BaseGeometry class for geometric shapes.
-
-This abstract class defines a blueprint for geometric shapes. It requires
-subclasses to implement the `area` method, which should calculate the area
-of the specific shape. The class cannot be instantiated directly.
-
-Attributes:
-    None
-
-Methods:
-    area() -- Raises an Exception indicating that the method is
-    not implemented.
-"""
+#!/usr/bin/python3s
+"""Define an geometry class"""
 
 
 class BaseGeometry:
     """Geometry class"""
-    def area(self):
-        """Calculate the area of the shape.
 
-        This method is meant to be overridden in subclasses to provide
-        specific implementations for calculating the area. If called
-        on an instance of BaseGeometry, it raises an Exception indicating
-        that the method is not implemented.
+    def area(self):
+        """
+        A public instance method.
 
         Raises:
-            Exception: If called directly without being overridden.
+            Exception: with the message area() is not implemented.
         """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """
-        Validate that the given value is a positive integer.
+        """Validates value.
 
-        Parameters:
-            name (str): The name of the variable being validated,
-            used for error messages.
-            value (any): The value to be validated.
+        Args:
+            name (str): You can assume is always a string.
+            value (int): value to validate.
 
         Raises:
-            TypeError: If `value` is not an integer.
-            ValueError: If `value` is less than or equal to 0.
-
-        Returns:
-            int: The validated integer value.
+            TypeError: if value is not an integer.
+            ValueError: if value <= 0.
         """
-        if not isinstance(value, int):
-            raise TypeError('{:s} must be an integer'.format(name))
+        if not isinstance(value, int) or isinstance(value, bool):
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError('{:s} must be greater than 0'.format(name))
-        return value
+            raise ValueError(f"{name} must be greater than 0")
