9cb25c6b4a839c0f4d88137cffb480d052e525b4
sigidibau <adriansantiago33@gail.com>
2024-09-26 11:27:39 -0400

    first abstract class

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
-"""_summary_"""
+"""This module defines an abstract base class for animals
+and provides concrete implementations for Dog and Cat classes.
+"""
+
 
 from abc import ABC, abstractmethod
 
 
 class Animal(ABC):
+    """Abstract base class representing an Animal."""
+
     @abstractmethod
     def sound(self):
+        """Return the sound that the animal makes."""
 
         class Dog(Animal):
+            """Class representing a Dog, which is a type of Animal."""
+
             def sound(self):
+                """Return the sound made by a Dog."""
+
                 return 'Bark'
 
         class Cat(Animal):
+            """Class representing a Cat, which is a type of Animal."""
+
             def sound(self):
+                """Return the sound made by a Cat."""
+
                 return 'Meow'
