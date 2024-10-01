284c375c1ca82e22a89dcf23cc1225cc1271f764
sigidibau <9571@holbertonstudents.com>
2024-10-01 11:35:00 -0400

    returning json object

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -8,4 +8,4 @@ import json
 def load_from_json_file(filename):
     """Reads a JSON file and returns the corresponding Python object."""
     with open(filename, 'r', encoding="utf-8") as f:
-        return json.loads(f)
+        return json.load(f)
