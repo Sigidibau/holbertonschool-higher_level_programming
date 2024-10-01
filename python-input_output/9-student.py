7f22aafd805aa8f29c431d36962f9252ab170b49
sigidibau <9571@holbertonstudents.com>
2024-10-01 12:30:54 -0400

    class and json

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -3,15 +3,12 @@
 
 
 class Student:
-    """Represents a student.
-
-    Attributes:
-        first_name (str): The first name of the student.
-        last_name (str): The last name of the student.
-        age (int): The age of the student.
-    """
+    """Represents a student."""
 
     def __init__(self, first_name, last_name, age):
+        """
+        Instantiation with first_name, last_name and age
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
