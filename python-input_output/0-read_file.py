8a63ae0b0a75f15899d3d486ab2def018b2bdfee
sigidibau <9571@holbertonstudents.com>
2024-10-01 09:38:14 -0400

    read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def read_file(filename=""):
+    """
+    Reads the content of a text file and returns it as a string.
+
+    Args:
+        filename (str): The path to the file to be read.
+                        Defaults to an empty string, which should be replaced
+                        with a valid file path when calling the function.
+    """
+    with open('workfile', 'w', encoding="utf-8") as f:
+        read_file = f.read
