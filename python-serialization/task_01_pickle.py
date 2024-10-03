7d314840f0811d093a619a44657602f782d9edb2
sigidibau <9571@holbertonstudents.com>
2024-10-03 18:13:44 -0400

    pickle

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import pickle
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+        
+        
+    def displa(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+        
+    def serialize(self, filename):
+        try:
+            with open(filename, 'wb') as f:
+                pickle.dump(self, f)
+        except Exception as e:
+            print(f"Error serializing the object: {e}")
+        return None
+    
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, 'rb') as file:
+                return pickle.load(file)
+        except FileNotFoundError:
+            print(f"File not found: {filename}")
+            return None
+        except Exception as e:
+            print(f"Error deserializing the object: {e}")
+            return None
