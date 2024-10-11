51bbca50bb9fb04b63fde7731dca33d80333b313
sigidibau <9571@holbertonstudents.com>
2024-10-11 09:43:21 -0400

    some fixes

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -9,8 +9,8 @@ def fetch_and_print_posts():
     r = requests.get("https://jsonplaceholder.typicode.com/posts")
     print(f"Status Code: {r.status_code}")
     if r.status_code == 200:
-        data = r.json()
-    for post in data:
+        posts = r.json()
+    for post in posts:
         print(post['title'])
     else:
         print("Failed to retrieve data", r.status_code)
@@ -19,10 +19,10 @@ def fetch_and_save_posts():
     r = requests.get("https://jsonplaceholder.typicode.com/posts")
     print(f"Status Code for saving: {r.status_code}")
     if r.status_code == 200:
-        data = r.json()
+        posts = r.json()
         d_list = []
 
-    for posts in data:
+    for posts in posts:
         d_list.append({
             'id': posts['id'],
             'title': posts['title'],
