22f2325b41dcac79f59da9139114ec72a6b44fbd
sigidibau <9571@holbertonstudents.com>
2024-10-11 09:41:00 -0400

    some fixes

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -10,25 +10,26 @@ def fetch_and_print_posts():
     print(f"Status Code: {r.status_code}")
     if r.status_code == 200:
         data = r.json()
-    for d in data:
-        print(d['title'])
+    for post in data:
+        print(post['title'])
     else:
         print("Failed to retrieve data", r.status_code)
 
 def fetch_and_save_posts():
     r = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print(f"Status Code for saving: {r.status_code}")
     if r.status_code == 200:
         data = r.json()
         d_list = []
 
-    for d in data:
+    for posts in data:
         d_list.append({
-            'id': d ['id'],
-            'title': d ['title'],
-            'body': d ['body']
+            'id': posts['id'],
+            'title': posts['title'],
+            'body': posts['body']
         })
 
-        with open('d.csv', 'w', newline='', encoding='utf-8') as file:
+        with open('posts.csv', 'w', encoding='utf-8') as file:
                 writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
                 writer.writeheader()
                 writer.writerows(d_list)
