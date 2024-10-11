09e7e1a331dbe5fe2e22932c22b731949cd206d1
sigidibau <9571@holbertonstudents.com>
2024-10-11 09:59:02 -0400

    some fixes

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
 
 
-import csv
 import requests
+import csv
 
 
 def fetch_and_print_posts():
-    r = requests.get("https://jsonplaceholder.typicode.com/posts")
-    print(f"Status Code: {r.status_code}")
-    if r.status_code == 200:
-        posts = r.json()
-    for post in posts:
-        print(post['title'])
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    print(f"Status Code: {response.status_code}")
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
     else:
-        print("Failed to retrieve data", r.status_code)
+        print(f"Failed to retrieve data {response.status_code}")
+
 
 def fetch_and_save_posts():
-    r = requests.get("https://jsonplaceholder.typicode.com/posts")
-    if r.status_code == 200:
-        posts = r.json()
-        d_list = []
-
-    for post in posts:
-        d_list.append({
-            'id': post['id'],
-            'title': post['title'],
-            'body': post['body']
-        })
-
-        with open('posts.csv', 'w', encoding='utf-8') as file:
-                writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
-                writer.writeheader()
-                writer.writerows(d_list)
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    if response.status_code == 200:
+        posts = response.json()
+        posts_data = [
+            {'id': post['id'], 'title': post['title'], 'body': post['body']}
+            for post in posts
+        ]
+        with open('posts.csv', "w", encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
+            writer.writeheader()
+            writer.writerows(posts_data)
