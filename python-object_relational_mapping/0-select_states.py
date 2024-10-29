ff596521ad7f8ba1d2bdb2a60cb69198f1dd391f
sigidibau <9571@holbertonstudents.com>
2024-10-29 11:08:39 -0400

    get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    
+
     con = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -19,13 +19,13 @@ if __name__ == "__main__":
         db=database,
         charset="utf8"
     )
-    
+
     cur = conn.cursor()
     cur.execute("SELECT * FROM state ORDER BY id")
-    query_rows =- cur.fetchall()
-    
+    query_rows = cur.fetchall()
+
     for row in query_rows:
         print(row)
-        
+
     cur.close()
     conn.close()
