b72ba87c98bdf7f452fa1670b2c278906e2e0f48
Sigidibau <adriansantiago33@gmail.com>
2024-10-30 10:12:22 -0400

    firs state

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+"""list the fist object"""
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, model_state
+from model_state import Base, State
 
 
 if __name__ == "__main__":
