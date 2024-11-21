c5fdbcabb9d0320251f7df963423865819d92e91
sigidibau <9571@holbertonstudents.com>
2024-11-20 20:27:04 -0400

    red class

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,5 @@
+const redHeaderButton = document.getElementById('red_header');
+redHeaderButton.addEventListener('click', function() {
+  const header = document.querySelector('h1');
+  header.classList.add('red');
+});
