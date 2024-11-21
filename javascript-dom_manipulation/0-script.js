ade31a7b1ad6c3905e3cae4dadb3ba718ce90cd5
sigidibau <9571@holbertonstudents.com>
2024-11-20 20:23:18 -0400

    colora change header

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -1,16 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Color Me</title>
-</head>
-<body>
-    <h1 id="header">This is a Header</h1>
-
-    <script>
-        // JavaScript to change the text color of the header to red
-        document.querySelector('h1').style.color = '#FF0000';
-    </script>
-</body>
-</html>
+document.querySelector('h1').style.color = '#FF0000';
