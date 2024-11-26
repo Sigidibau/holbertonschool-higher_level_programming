f70ff44c9a9c3a438577bdfdd037f3f5e6809280
Sigidibau <adriansantiago33@gmail.com>
2024-11-26 10:01:59 -0400

    h1 for header

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,6 +1,6 @@
 const redHeaderButton = document.getElementById('red_header');
 
 redHeaderButton.addEventListener('click', function() {
-  const header = document.querySelector('h1');
+  const header = document.querySelector('header');
   header.style.color = '#FF0000';
 });
